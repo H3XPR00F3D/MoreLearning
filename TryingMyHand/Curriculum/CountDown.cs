311125420a8aafcb7b2025c6b76bcb20ce105ac3
H3XPR00F3D <david@ddornart.com>
2022-12-23 02:30:35 -0500

    created an age countdown to 100yrs. Learned about lists Made a network pinger from tutorial.

---
+++ TryingMyHand/Curriculum/CountDown.cs
@@ -0,0 +1,36 @@
+﻿/*
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreLearning.Curriculum
+{
+    public class CountDown
+    {
+        public static void Main(string[] args)
+        {
+            string name;
+            int age;
+
+            Console.Write("What is your name?");
+            name = Console.ReadLine();
+
+            Console.Write("What is your age?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            int countdown = 100 - age;
+
+            Console.WriteLine(name + " you have " + countdown.ToString() + " more years until you are 100!!");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
+*/
---
+++ TryingMyHand/ListTut/Lists.cs
@@ -0,0 +1,31 @@
+﻿/*
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoreLearning;
+
+
+namespace MoreLearning
+{
+    public class Lists
+    {
+        public static void Main(string[] args)
+        {
+            List<Part> lst = new List<Part>();
+
+            lst.Add(new Part{ PartName = "Headlight", PartID = 1 });
+            lst.Add(new Part { PartName = "Bumper", PartID = 2 });
+
+            foreach (Part part in lst)
+            {
+                Console.WriteLine(part.PartName);
+                Console.WriteLine(part.PartID.ToString());
+            }
+        }
+    }
+}
+
+*/
